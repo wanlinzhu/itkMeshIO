247c34c99cab2c9d2e6c33d886b4f38f22115b25
Wanlin Zhu <wanl.zhu@gmail.com>
2010-11-27 01:35:57 +1100

    change to itk v4 license

--- MeshIO/itkMeshFileWriter.h
+++ MeshIO/itkMeshFileWriter.h
@@ -1,19 +1,20 @@
 /*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkMeshFileWriter.h,v $
-  Language:  C++
-  Date:      $Date: 2010-11-17 22:42:10 $
-  Version:   $Revision: 0.04 $
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #ifndef __itkMeshFileWriter_h
 #define __itkMeshFileWriter_h
 
@@ -63,7 +64,7 @@ public:
  * with a suitable suffix (".vtk", etc) and setting the input
  * to the writer is enough to get the writer to work properly.
  *
- * \author wanlin zhu   wanl.zhu@gmail.com
+ * \author Wanlin Zhu. Uviversity of New South Wales, Australia.
  *
  * \sa MeshIOBase
  *
--- MeshIO/itkVTKPolyDataMeshIO.h
+++ MeshIO/itkVTKPolyDataMeshIO.h
@@ -1,19 +1,20 @@
 /*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkVTKPolyDataMeshIO.h,v $
-  Language:  C++
-  Date:      $Date: 2010-09-06 21:52:40 $
-  Version:   $Revision: 0.10 $
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #ifndef __itkVTKPolyDataMeshIO_h
 #define __itkVTKPolyDataMeshIO_h
 
@@ -34,9 +35,9 @@ namespace itk
 {
 /** \class VTKPolyDataMeshIO
  * \brief
- * Reads a vtkPolyData file and create an itk::Mesh<> or itk::QuadEdgeMesh<>
+ * Reads a vtkPolyData legacy file and create an itk::Mesh<> or itk::QuadEdgeMesh<>
  *
- * \author wanlin zhu   wanl.zhu@gmail.com
+ * \author Wanlin Zhu. Uviversity of New South Wales, Australia.
  */
 class ITK_EXPORT VTKPolyDataMeshIO:public MeshIOBase
 {
