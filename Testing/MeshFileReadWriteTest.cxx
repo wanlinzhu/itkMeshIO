86474a954b6afde56cdb10257853cb76d872e54c
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-04-04 14:39:54 -0400

    STYLE: Add missing copyright headers

--- Testing/MeshFileReadWriteTest.cxx
+++ Testing/MeshFileReadWriteTest.cxx
@@ -1,3 +1,21 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 #include "itkMesh.h"
 #include "itkQuadEdgeMesh.h"
 #include "itksys/SystemTools.hxx"
@@ -12,11 +30,7 @@ int main(int argc, char ** argv)
   return EXIT_FAILURE;
   }
 
-  bool IsBinary = false;
-  if( argc > 3 )
-    {
-    IsBinary = true;
-    }
+  bool IsBinary = ( argc > 3 );
 
   const unsigned int dimension = 3;
   typedef float PixelType;
@@ -24,9 +38,15 @@ int main(int argc, char ** argv)
   typedef itk::Mesh<PixelType, dimension>            MeshType;
   typedef itk::QuadEdgeMesh<PixelType, dimension>    QEMeshType;
 
-  if( test< MeshType   >( argv[1], argv[2], IsBinary ) ) return EXIT_FAILURE;
-  if( test< QEMeshType >( argv[1], argv[2], IsBinary ) ) return EXIT_FAILURE;
- 
+  if( test< MeshType   >( argv[1], argv[2], IsBinary ) )
+    {
+    return EXIT_FAILURE;
+    }
+  if( test< QEMeshType >( argv[1], argv[2], IsBinary ) )
+    {
+    return EXIT_FAILURE;
+    }
+
   return EXIT_SUCCESS;
 
 }
--- Testing/MeshFileTestHelper.h
+++ Testing/MeshFileTestHelper.h
@@ -1,3 +1,21 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 #include "itkMeshFileReader.h"
 #include "itkMeshFileWriter.h"
 #include <itksys/SystemTools.hxx>
@@ -24,13 +42,14 @@ test(char *INfilename, char *OUTfilename, bool IsBinary)
     return EXIT_FAILURE;
     }
 
-  typename MeshFileWriterType::Pointer writer = MeshFileWriterType::New();
-	if(itksys::SystemTools::GetFilenameLastExtension(INfilename) == itksys::SystemTools::GetFilenameLastExtension(OUTfilename) )
-    {
-    writer->SetMeshIO(reader->GetMeshIO());
-    }
-  writer->SetFileName(OUTfilename);
-  writer->SetInput( reader->GetOutput() );
+	typename MeshFileWriterType::Pointer writer = MeshFileWriterType::New();
+	if( itksys::SystemTools::GetFilenameLastExtension(INfilename) ==
+			itksys::SystemTools::GetFilenameLastExtension(OUTfilename) )
+		{
+		writer->SetMeshIO(reader->GetMeshIO());
+		}
+	writer->SetFileName(OUTfilename);
+	writer->SetInput( reader->GetOutput() );
 
   // NOTE ALEX: we should document the usage
   if ( IsBinary )
